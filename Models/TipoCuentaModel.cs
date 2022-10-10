0ef963f074cd2720dcb4410a6d2e1ba978bc2708
Fernando Nicolás <fersanti2896@gmail.com>
2022-10-10 09:30:42 -0500

    Verificando tag helpers en Crear.cshtml

---
+++ Models/TipoCuentaModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ManejoPresupuesto.Models {
+    public class TipoCuentaModel {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [StringLength(maximumLength: 50, MinimumLength = 3, ErrorMessage = "La longitud del campo {0} debe ser entre {2} y {1}")]
+        [Display(Name = "Nombre del tipo cuenta")]
+        public string Nombre { get; set; }
+        public int UsuarioID { get; set; }
+        public int Orden { get; set; }
+    }
+}
